cc9e8b3a9a3453b4a7a575d60ae701a6ea039d64
Claudio Ribeiro <claudio.ribeiro@senff1.onmicrosoft.com>
2022-11-10 17:50:21 -0300

    Teste criado para validar o processo de faturamento

---
+++ Alura.Estacionamento.Teste/PatioTeste.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+
+namespace Alura.Estacionamento.Teste
+{
+    public class PatioTeste
+    {
+        [Fact]
+        public void ValidaFaturamento()
+        {
+            //Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo();
+            veiculo.Proprietario = "Claudio Ribeiro";
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Prata";
+            veiculo.Modelo = "Celta";
+            veiculo.Placa = "DSM-8637";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+        }
+    }
+}
